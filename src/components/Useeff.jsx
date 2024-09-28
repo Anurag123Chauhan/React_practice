2cd52c3b4b1fe83977b62a831c1a96cd2837102a
Anurag Chauhan <chauhananurag322@gmail.com>
2024-09-28 12:18:36 +0530

    commit

--- src/components/Useeff.jsx
+++ src/components/Useeff.jsx
@@ -1,36 +1,52 @@
 import { useEffect, useState } from "react";
 
 export default function Useeff() {
-  const [resourseType, setResourseType] = useState("post");
-  useEffect(() => {
-    console.log("resource changed");
-  }, [resourseType]);
-  return (
-    <>
-      <div>
-        <button
-          onClick={() => {
-            setResourseType("Post");
-          }}
-        >
-          Post
-        </button>
-        <button
-          onClick={() => {
-            setResourseType("Users");
-          }}
-        >
-          Users
-        </button>
-        <button
-          onClick={() => {
-            setResourseType("Comments");
-          }}
-        >
-          Comments
-        </button>
-      </div>
-      <h1>{resourseType}</h1>
-    </>
-  );
+//   const [resourseType, setResourseType] = useState("post");
+//   useEffect(() => {
+//     console.log("resource changed");
+//   }, [resourseType]);
+//   return (
+//     <>
+//       <div>
+//         <button
+//           onClick={() => {
+//             setResourseType("Post");
+//           }}
+//         >
+//           Post
+//         </button>
+//         <button
+//           onClick={() => {
+//             setResourseType("Users");
+//           }}
+//         >
+//           Users
+//         </button>
+//         <button
+//           onClick={() => {
+//             setResourseType("Comments");
+//           }}
+//         >
+//           Comments
+//         </button>
+//       </div>
+//       <h1>{resourseType}</h1>
+//     </>
+//   );
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+    useEffect(()=>{
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        }
+    }, []);
+    return (
+        <div>
+            <h1>{windowWidth}</h1>
+        </div>
+    );
 }
